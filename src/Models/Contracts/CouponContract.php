5ee20403d24b57530463bd06518253ac783008fc
Michael Rubel <contact@observer.name>
2022-03-09 12:51:46 +0100

    apply functional signatures

--- src/Models/Contracts/CouponContract.php
+++ src/Models/Contracts/CouponContract.php
@@ -4,8 +4,68 @@ declare(strict_types=1);
 
 namespace MichaelRubel\Couponables\Models\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface CouponContract
 {
+    /*
+    | Functional definitions.
+    |
+    | You may implement the methods in the way you wish.
+    */
+
+    /**
+     * The only model allowed to redeem the code if assigned.
+     *
+     * @return Model|null
+     */
+    public function redeemer(): ?Model;
+
+    /**
+     * Check if code is expired.
+     *
+     * @return bool
+     */
+    public function isExpired(): bool;
+
+    /**
+     * Check if code is not expired.
+     *
+     * @return bool
+     */
+    public function isNotExpired(): bool;
+
+    /**
+     * Check if code amount is over.
+     *
+     * @return bool
+     */
+    public function isOverQuantity(): bool;
+
+    /**
+     * Check if coupon is already redeemed by the model.
+     *
+     * @param Model $redeemer
+     *
+     * @return bool
+     */
+    public function isRedeemedBy(Model $redeemer): bool;
+
+    /**
+     * Check if the code is reached its limit for the passed model.
+     *
+     * @param Model $redeemer
+     *
+     * @return bool
+     */
+    public function isOverLimitFor(Model $redeemer): bool;
+
+    /*
+    | Column definitions.
+    |
+    | For package's internal purposes.
+    */
+
     /**
      * @return string
      */
