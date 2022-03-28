9aed403a62cfc47388b1ee020931a5ead6486ce4
Michał Rubel <contact@observer.name>
2022-03-28 18:11:48 +0200

    add macroable test

---
+++ tests/MacroabilityTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MichaelRubel\Couponables\Tests;
+
+use MichaelRubel\Couponables\Models\Coupon;
+use MichaelRubel\Couponables\Services\Contracts\CouponServiceContract;
+use MichaelRubel\Couponables\Services\CouponService;
+
+class MacroabilityTest extends TestCase
+{
+    /**
+     * @var void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Coupon::create([
+            'code' => 'coupon',
+            'type' => 'macro',
+        ]);
+    }
+
+    /** @test */
+    public function testCanMacroIntoTheService()
+    {
+        CouponService::macro('getCouponUsing', function (string $column, string $value) {
+            return $this->model
+                ->where($this->model->{'get' . ucfirst($column) . 'Column'}(), $value)
+                ->first();
+        });
+
+        $coupon = call(CouponServiceContract::class)
+            ->getCouponUsing('type', 'macro');
+
+        $this->assertInstanceOf(Coupon::class, $coupon);
+    }
+}
