c5bca21ba682a62b4cc904a28f09cb2efda9660b
Michał Rubel <contact@observer.name>
2022-05-26 19:13:06 +0200

    add relation to the coupon

--- src/Models/Couponable.php
+++ src/Models/Couponable.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace MichaelRubel\Couponables\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use MichaelRubel\Couponables\Models\Contracts\CouponPivotContract;
 use MichaelRubel\Couponables\Models\Traits\DefinesPivotColumns;
 
@@ -20,5 +22,13 @@ class Couponable extends MorphPivot implements CouponPivotContract
         parent::__construct($attributes);
 
         $this->table = config('couponables.pivot_table', 'couponables');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function coupon(): BelongsTo
+    {
+        return $this->belongsTo(config('couponables.model', Coupon::class));
     }
 }
