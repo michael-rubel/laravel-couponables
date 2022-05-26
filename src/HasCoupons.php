ba4320fe25e8330e7a5808438d11da1c0b7fe91f
Michał Rubel <contact@observer.name>
2022-05-26 18:12:17 +0200

    update the trait

--- src/HasCoupons.php
+++ src/HasCoupons.php
@@ -49,84 +49,6 @@ trait HasCoupons
     }
 
     /**
-     * Perform coupon verification.
-     *
-     * @param string|null $code
-     *
-     * @return CouponContract
-     */
-    public function verifyCoupon(?string $code): CouponContract
-    {
-        return self::$bindableService->verifyCoupon($code, $this);
-    }
-
-    /**
-     * Verify the coupon or do something else on fail.
-     *
-     * @param string|null $code
-     * @param mixed|null $callback
-     * @param bool $report
-     *
-     * @return mixed
-     */
-    public function verifyCouponOr(?string $code, mixed $callback = null, bool $report = false): mixed
-    {
-        return rescue(
-            callback: fn () => self::$bindable->verifyCoupon($code),
-            rescue: $callback,
-            report: $report
-        );
-    }
-
-    /**
-     * Use the coupon.
-     *
-     * @param string|null $code
-     * @param Model|null  $redeemed
-     *
-     * @return CouponContract
-     */
-    public function redeemCoupon(?string $code, ?Model $redeemed = null): CouponContract
-    {
-        $coupon = self::$bindableService->verifyCoupon($code, $this);
-
-        return self::$bindableService->applyCoupon($coupon, $this, $redeemed);
-    }
-
-    /**
-     * Redeem the code using model.
-     *
-     * @param Model       $model
-     * @param string|null $couponCode
-     *
-     * @return CouponContract
-     */
-    public function redeemBy(Model $model, ?string $couponCode): CouponContract
-    {
-        $coupon = self::$bindableService->verifyCoupon($couponCode, $model);
-
-        return self::$bindableService->applyCoupon($coupon, $model, $this);
-    }
-
-    /**
-     * Redeem the coupon or do something else on fail.
-     *
-     * @param string|null $code
-     * @param mixed|null $callback
-     * @param bool $report
-     *
-     * @return mixed
-     */
-    public function redeemCouponOr(?string $code, mixed $callback = null, bool $report = false): mixed
-    {
-        return rescue(
-            callback: fn () => self::$bindable->redeemCoupon($code, null),
-            rescue: $callback,
-            report: $report
-        );
-    }
-
-    /**
      * Check if coupon with this code is already used.
      *
      * @param string|null $code
@@ -155,5 +77,85 @@ trait HasCoupons
         $coupon = self::$bindableService->getCoupon($code);
 
         return ! is_null($coupon) && call($coupon)->isOverLimitFor($this);
+    }
+
+    /**
+     * Verify if the coupon is valid.
+     *
+     * @param string|null $code
+     *
+     * @return CouponContract
+     */
+    public function verifyCoupon(?string $code): CouponContract
+    {
+        return self::$bindableService->verifyCoupon($code, $this);
+    }
+
+    /**
+     * Verify and use the coupon.
+     *
+     * @param string|null $code
+     * @param Model|null  $redeemed
+     *
+     * @return CouponContract
+     */
+    public function redeemCoupon(?string $code, ?Model $redeemed = null): CouponContract
+    {
+        return with(self::$bindableService, function ($service) use ($code, $redeemed) {
+            $coupon = $service->verifyCoupon($code, $this);
+
+            return $service->applyCoupon($coupon, $this, $redeemed);
+        });
+    }
+
+    /**
+     * Verify the coupon or do something else on fail.
+     *
+     * @param string|null $code
+     * @param mixed|null  $callback
+     *
+     * @return mixed
+     */
+    public function verifyCouponOr(?string $code, mixed $callback = null): mixed
+    {
+        return rescue(
+            callback: fn () => self::$bindable->verifyCoupon($code),
+            rescue: $callback,
+            report: false
+        );
+    }
+
+    /**
+     * Redeem the coupon or do something else on fail.
+     *
+     * @param string|null $code
+     * @param mixed|null  $callback
+     *
+     * @return mixed
+     */
+    public function redeemCouponOr(?string $code, mixed $callback = null): mixed
+    {
+        return rescue(
+            callback: fn () => self::$bindable->redeemCoupon($code, null),
+            rescue: $callback,
+            report: false
+        );
+    }
+
+    /**
+     * Redeem the code using model.
+     *
+     * @param Model       $model
+     * @param string|null $couponCode
+     *
+     * @return CouponContract
+     */
+    public function redeemBy(Model $model, ?string $couponCode): CouponContract
+    {
+        return with(self::$bindableService, function ($service) use ($couponCode, $model) {
+            $coupon = $service->verifyCoupon($couponCode, $model);
+
+            return $service->applyCoupon($coupon, $model, $this);
+        });
     }
 }
