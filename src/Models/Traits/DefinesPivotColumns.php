92f3823b17d2afa6fe49f6bf7376c0ba05367433
Michael Rubél <contact@observer.name>
2024-03-15 21:48:32 +0100

    `pint` fixes

--- src/Models/Traits/DefinesPivotColumns.php
+++ src/Models/Traits/DefinesPivotColumns.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MichaelRubel\Couponables\Models\Traits;
 
 trait DefinesPivotColumns
--- src/Services/CouponService.php
+++ src/Services/CouponService.php
@@ -67,7 +67,7 @@ class CouponService implements CouponServiceContract
      * @throws CouponDisabledException
      * @throws NotAllowedToRedeemException
      */
-    public function verifyCoupon(?string $code, Model $redeemer = null): CouponContract
+    public function verifyCoupon(?string $code, ?Model $redeemer = null): CouponContract
     {
         $coupon = $this->getCoupon($code) ?? throw new InvalidCouponException;
 
@@ -96,7 +96,7 @@ class CouponService implements CouponServiceContract
      * @throws CouponExpiredException
      * @throws CouponDisabledException
      */
-    public function performBasicChecksOn(?CouponContract $coupon, Model $redeemer = null): CouponContract
+    public function performBasicChecksOn(?CouponContract $coupon, ?Model $redeemer = null): CouponContract
     {
         if (! $coupon) {
             throw new InvalidCouponException;
